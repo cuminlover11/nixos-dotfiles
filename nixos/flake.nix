38aa5795c42f2aa676162d10a913117f9cf9292a
cuminlover11 <youssefelmhini@protonmail.com>
2026-07-26 14:07:25 +0100

    restructure: repo root at dofiles/

---
+++ nixos/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "tunix nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+    home-manager = {
+	url = "github:nix-community/home-manager/release-26.05";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.tunix = nixpkgs.lib.nixosSystem {
+	system = "x86_64-linux";
+	modules = [
+		./configuration.nix
+	];
+    };
+  };
+}
